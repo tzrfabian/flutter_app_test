1588e12198558800c08afec9ca39c4750a3eef76
Fabian Tenzara <fabiantenzara@gmail.com>
2025-09-02 08:14:35 +0700

    refac: fix main & firebase api(not solved yet)

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,5 +1,12 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+    // Handle background message
+    print("Notif Title: ${message.notification?.title}");
+    print("Notif Body: ${message.notification?.body}");
+    print("Notif Payload: ${message.data}");
+  }
+
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
@@ -7,5 +14,6 @@ class FirebaseApi {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('Token: $fCMToken');
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 }
