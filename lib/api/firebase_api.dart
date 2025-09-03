e05e2bdccb3392fd8ebbbb234266a700fd10200a
Fabian Tenzara <fabiantenzara@gmail.com>
2025-09-03 09:34:43 +0700

    refac: update api and main

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_app_test/main.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 @pragma('vm:entry-point')
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
@@ -6,27 +10,98 @@ Future<void> handleBackgroundMessage(RemoteMessage message) async {
     print("Notif Title: ${message.notification?.title}");
     print("Notif Body: ${message.notification?.body}");
     print("Notif Payload: ${message.data}");
-  }
+}
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
+
+  // Initialize the FlutterLocalNotificationsPlugin
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.defaultImportance,
+  );
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  void handleNotif(RemoteMessage? message) {
+    if (message == null) return;
+
+    navigatorKey.currentState?.pushNamed(
+      '/products',
+      arguments: message,
+    );
+  }
+
+  Future initLocalNotifications() async {
+    const iOS = DarwinInitializationSettings();
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android, iOS: iOS);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse notificationResponse) async {
+        final payload = notificationResponse.payload;
+        if (payload != null) {
+          final message = RemoteMessage.fromMap(jsonDecode(payload));
+          handleNotif(message);
+        }
+      },
+    );
+  }
+
+  Future initPushNotifications() async {
+    await FirebaseMessaging.instance
+    .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.instance.getInitialMessage().then(handleNotif);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleNotif);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message){
+      final notifications = message.notification; // Get the notification details
+      // Handle foreground messages
+      if(notifications == null) return; // if notifications is null, do nothing
+
+      // Show the notification
+      _localNotifications.show(
+        notifications.hashCode,
+        notifications.title,
+        notifications.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+  }
 
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('FCM Token: $fCMToken');
-    // FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-      if(message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-    });
+    initPushNotifications();
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   print('Got a message whilst in the foreground!');
+    //   print('Message data: ${message.data}');
+    //   if(message.notification != null) {
+    //     print('Message also contained a notification: ${message.notification}');
+    //   }
+    // });
 
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('🔥 NOTIFICATION TAPPED 🔥');
-      print('Data: ${message.data}');
-    });
+    // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    //   print('🔥 NOTIFICATION TAPPED 🔥');
+    //   print('Data: ${message.data}');
+    // });
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_app_test/api/firebase_api.dart';
 import 'app.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter bindings are initialized
   await Firebase.initializeApp();
   await FirebaseApi().initNotifications();
   runApp(const MyApp());
